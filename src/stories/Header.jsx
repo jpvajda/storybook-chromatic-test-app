72b26b3c222731f3820081cda854387bfa558afb
John P. Vajda <john.vajda@gmail.com>
2023-01-17 11:42:30 -0700

    makes it green

--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -20,7 +20,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
             />
             <path
               d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
-              fill= "#FFFF00"
+              fill= "#00FF00"
             />
           </g>
         </svg>
